46e44a0a95127d97dc1dc3c869936d058dd79335
prime8 <prime8@dev.java.net>
2010-04-26 00:22:54 +0000

    Test build for existing fitnesse instance and for exploding the fitnesse jar file

--- src/test/java/hudson/plugins/fitnesse/HudsonDependentTest.java
+++ src/test/java/hudson/plugins/fitnesse/HudsonDependentTest.java
@@ -1,5 +1,6 @@
 package hudson.plugins.fitnesse;
 
+import hudson.FilePath;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
@@ -24,7 +25,7 @@ public class HudsonDependentTest extends HudsonTestCase {
 		Assert.assertSame(parent.getOwner(), child.getOwner());
 	}
 	
-	public void testProjectStartingFitnesseWithAbsoluteAndRelativePaths() throws Exception {
+	public void testBuildStartingFitnesseWithAbsoluteAndRelativePaths() throws Exception {
 		FreeStyleProject project = createFreeStyleProject(getName());
 		project.getBuildersList().clear();
 		project.getPublishersList().clear();
@@ -32,10 +33,8 @@ public class HudsonDependentTest extends HudsonTestCase {
 
 		Map<String, String> options = new HashMap<String, String>();
 		options.put(FitnesseBuilder.START_FITNESSE, Boolean.TRUE.toString());
-		options.put(FitnesseBuilder.PATH_TO_JAR, 
-				new File(new File(System.getProperty("user.dir")), "target/test-classes/fitnesse.jar").getAbsolutePath());
-		options.put(FitnesseBuilder.PATH_TO_ROOT, 
-				new File(new File(System.getProperty("user.dir")), "target/test-classes/FitNesseRoot").getAbsolutePath());
+		options.put(FitnesseBuilder.PATH_TO_JAR, getTestResourceFitnesseJar());
+		options.put(FitnesseBuilder.PATH_TO_ROOT, getTestResourceFitNesseRoot());
 		options.put(FitnesseBuilder.FITNESSE_PORT, "8081");
 		options.put(FitnesseBuilder.TARGET_PAGE, "HudsonPlugin.SuiteAll");
 		options.put(FitnesseBuilder.TARGET_IS_SUITE, Boolean.TRUE.toString());
@@ -44,7 +43,7 @@ public class HudsonDependentTest extends HudsonTestCase {
 		
 		project.getBuildersList().add(builder);
 		project.getPublishersList().add(new FitnesseResultsRecorder(resultsFile));
-		System.out.println("!!! AbsolutePath build !!!");
+		//AbsolutePath build 
 		FreeStyleBuild build = project.scheduleBuild2(0).get();
 		Assert.assertTrue(build.getLogFile().getAbsolutePath(), !Result.FAILURE.equals(build.getResult()));
 		FitnesseResultsAction resultsAction = build.getAction(FitnesseResultsAction.class);
@@ -62,17 +61,90 @@ public class HudsonDependentTest extends HudsonTestCase {
 
 		project.getBuildersList().add(builder);
 		project.getPublishersList().add(new FitnesseResultsRecorder(resultsFile));
-		System.out.println("!!! RelativePath build !!!");
+		//RelativePath build 
 		build = project.scheduleBuild2(0).get();
 		Assert.assertTrue(build.getLogFile().getAbsolutePath(), !Result.FAILURE.equals(build.getResult()));
 		resultsAction = build.getAction(FitnesseResultsAction.class);
 		assertExpectedResults(resultsAction);
 	}
 
+	private String getTestResourceFitnesseJar() {
+		return new File(new File(System.getProperty("user.dir")), "target/test-classes/fitnesse.jar").getAbsolutePath();
+	}
+	
+	private String getTestResourceFitNesseRoot() {
+		return new File(new File(System.getProperty("user.dir")), "target/test-classes/FitNesseRoot").getAbsolutePath();
+	}
+	
 	private void assertExpectedResults(FitnesseResultsAction resultsAction) {
 		Assert.assertEquals("passed", 1, resultsAction.getResult().getPassCount());
 		Assert.assertEquals("failed", 1, resultsAction.getResult().getFailCount());
 		Assert.assertEquals("ignored", 1, resultsAction.getResult().getIgnoredCount());
 		Assert.assertEquals("exceptions", 1, resultsAction.getResult().getExceptionCount());
+	}
+
+	public void testBuildForStartedFitnesse() throws Exception {
+		File workingDir = new File(getTestResourceFitNesseRoot()).getParentFile();
+		String[] commands = new String[] {"java", "-jar", getTestResourceFitnesseJar(), 
+			"-d", workingDir.getPath(),
+			"-r", "FitNesseRoot", "-p", "8082"};
+		Process process = new ProcessBuilder(commands).directory(workingDir).start();
+		Thread.sleep(1500);
+		try {
+			FreeStyleProject project = createFreeStyleProject(getName());
+			project.getBuildersList().clear();
+			project.getPublishersList().clear();
+			String resultsFile = "fitnesse-results3.xml";
+			
+			Map<String, String> options = new HashMap<String, String>();
+			options.put(FitnesseBuilder.START_FITNESSE, Boolean.FALSE.toString());
+			options.put(FitnesseBuilder.FITNESSE_HOST, "localhost");
+			options.put(FitnesseBuilder.FITNESSE_PORT, "8082");
+			options.put(FitnesseBuilder.TARGET_PAGE, "HudsonPlugin.SuiteAll");
+			options.put(FitnesseBuilder.TARGET_IS_SUITE, Boolean.TRUE.toString());
+			options.put(FitnesseBuilder.PATH_TO_RESULTS, resultsFile);
+			FitnesseBuilder builder = new FitnesseBuilder(options);
+			
+			project.getBuildersList().add(builder);
+			project.getPublishersList().add(new FitnesseResultsRecorder(resultsFile));
+			FreeStyleBuild build = project.scheduleBuild2(0).get();
+			Assert.assertTrue(build.getLogFile().getAbsolutePath(), !Result.FAILURE.equals(build.getResult()));
+			FitnesseResultsAction resultsAction = build.getAction(FitnesseResultsAction.class);
+			assertExpectedResults(resultsAction);
+		} finally {
+			process.destroy();
+		}
+	}
+	
+	public void testBuildStartingFitnesseAndExplodingTheJarFile() throws Exception {
+		FreeStyleProject project = createFreeStyleProject(getName());
+		project.getBuildersList().clear();
+		project.getPublishersList().clear();
+		String resultsFile = "fitnesse-results4.xml";
+		project.getBuildersList().add(new Shell("echo")); // only want this to get workspace
+		FreeStyleBuild build = project.scheduleBuild2(0).get();
+		FilePath workspace = build.getWorkspace();
+		
+		Map<String, String> options = new HashMap<String, String>();
+		options.put(FitnesseBuilder.START_FITNESSE, Boolean.TRUE.toString());
+		options.put(FitnesseBuilder.PATH_TO_JAR, workspace.child("fitnesse.jar").getRemote());
+		options.put(FitnesseBuilder.PATH_TO_ROOT, workspace.getRemote());
+		options.put(FitnesseBuilder.FITNESSE_PORT, "8083");
+		options.put(FitnesseBuilder.TARGET_PAGE, "FitNesse.SuiteAcceptanceTests.SuiteFixtureTests.SuiteColumnFixtureSpec.TestMissingMethod");
+		options.put(FitnesseBuilder.TARGET_IS_SUITE, Boolean.FALSE.toString());
+		options.put(FitnesseBuilder.PATH_TO_RESULTS, resultsFile);
+		FitnesseBuilder builder = new FitnesseBuilder(options);
+		
+		project.getBuildersList().clear();
+		project.getPublishersList().clear();
+		project.getBuildersList().add(new Shell("cp " + getTestResourceFitnesseJar() + " " + workspace.getRemote()));
+		project.getBuildersList().add(builder);
+		project.getPublishersList().add(new FitnesseResultsRecorder(resultsFile));
+		
+		build = project.scheduleBuild2(0).get();
+		Assert.assertTrue(build.getLogFile().getAbsolutePath(), !Result.FAILURE.equals(build.getResult()));
+		FitnesseResultsAction resultsAction = build.getAction(FitnesseResultsAction.class);
+		Assert.assertTrue(resultsAction.getResult().getPassCount() > 0);
+		Assert.assertEquals(0, resultsAction.getResult().getFailCount());
 	}
 }
