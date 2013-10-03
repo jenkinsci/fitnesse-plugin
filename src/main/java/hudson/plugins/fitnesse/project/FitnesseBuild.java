d47c4e7b83fc400cfd4a2633fb9f54fd0cf9a1cf
Arnout Engelen <arnouten@bzzt.net>
2013-10-03 15:54:58 +0200

    Better method name for git-specific method

--- src/main/java/hudson/plugins/fitnesse/project/FitnesseBuild.java
+++ src/main/java/hudson/plugins/fitnesse/project/FitnesseBuild.java
@@ -8,15 +8,15 @@ import hudson.model.Build;
 import hudson.plugins.fitnesse.project.FitnesseProject;
 
 public class FitnesseBuild extends Build<FitnesseProject,FitnesseBuild> {
-    protected FitnesseBuild(FitnesseProject project) throws IOException {
+    public FitnesseBuild(FitnesseProject project) throws IOException {
         super(project);
     }
 
-    protected FitnesseBuild(FitnesseProject project, File buildDir) throws IOException {
+    public FitnesseBuild(FitnesseProject project, File buildDir) throws IOException {
         super(project, buildDir);
     }
 
-    protected FitnesseBuild(FitnesseProject job, Calendar timestamp) {
+    public FitnesseBuild(FitnesseProject job, Calendar timestamp) {
         super(job, timestamp);
     }
 
